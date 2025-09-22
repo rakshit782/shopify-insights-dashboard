ea75d940a15fb343574775e857a8a40213ec4358
AMZ Ad Scout <globalads@amzadscout.com>
2025-09-22 21:53:58 +0000

    on signup, it is not adding the data added in the signup form to supabas

--- src/app/auth/actions.ts
+++ src/app/auth/actions.ts
@@ -26,6 +26,8 @@ export async function signup(formData: FormData) {
   const origin = headers().get('origin')
   const email = formData.get('email') as string
   const password = formData.get('password') as string
+  const firstName = formData.get('first-name') as string
+  const lastName = formData.get('last-name') as string
   const supabase = createClient()
 
   const { error } = await supabase.auth.signUp({
@@ -33,11 +35,16 @@ export async function signup(formData: FormData) {
     password,
     options: {
       emailRedirectTo: `${origin}/auth/callback`,
+      data: {
+        first_name: firstName,
+        last_name: lastName,
+      }
     },
   })
 
   if (error) {
-    return redirect('/login?message=Could not authenticate user')
+    console.error('Signup error:', error);
+    return redirect('/signup?message=Could not create user account. Please try again.')
   }
 
   return redirect('/login?message=Check email to continue sign in process')
