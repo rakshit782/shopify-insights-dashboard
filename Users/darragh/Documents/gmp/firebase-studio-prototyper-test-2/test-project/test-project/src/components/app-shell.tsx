fb29080fa0a6b8b23b3df2e6d4eb3339b295a5ec
AMZ Ad Scout <globalads@amzadscout.com>
2025-09-22 20:45:46 +0000

    it should not replace the changes i suggested in codes previously, becau

--- Users/darragh/Documents/gmp/firebase-studio-prototyper-test-2/test-project/test-project/src/components/app-shell.tsx
+++ Users/darragh/Documents/gmp/firebase-studio-prototyper-test-2/test-project/test-project/src/components/app-shell.tsx
@@ -13,7 +13,7 @@ import {
   SidebarFooter,
 } from '@/components/ui/sidebar';
 import { usePathname } from 'next/navigation';
-import { BarChartHorizontal, Globe, Settings, ShoppingCart, Truck, ListTodo, ShieldCheck } from 'lucide-react';
+import { BarChartHorizontal, Globe, Settings, ShoppingCart, Truck, ListTodo, ShieldCheck, FileText } from 'lucide-react';
 import Link from 'next/link';
 import Image from 'next/image';
 
