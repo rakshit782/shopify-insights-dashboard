afa0e25021ad371f90ccdef4214e44db70dcbfd5
AMZ Ad Scout <globalads@amzadscout.com>
2025-09-24 20:07:28 +0000

    how to change the color of dashboard to light as default.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           <SidebarProvider>
             <AppShell>
               {children}
