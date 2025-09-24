b47c78d1042554962bd42ec44a925edb05c701b0
AMZ Ad Scout <globalads@amzadscout.com>
2025-09-24 07:34:14 +0000

    when the app will start, everryone will land on analytics page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 
-import { HomeDashboard } from '@/components/home-dashboard';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <div className="p-4 sm:p-6 lg:p-8">
-      <HomeDashboard />
-    </div>
-  );
+  redirect('/analytics');
+  return null;
 }
